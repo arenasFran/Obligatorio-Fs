c6bcd5adc81e4aa1fbbb87f1b65a0d6f165ccb19
MacDuki <pcmyact@hotmail.com>
2025-10-03 00:25:27 -0300

    solve merge conflict

--- v1/config/db.js
+++ v1/config/db.js
@@ -3,9 +3,9 @@ import seedCollectionsForUsers from "../initCollections.js";
 import seedPlans from "../initPlans.js";
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI_DEV_Local);
-    //await seedPlans();
-    //await seedCollectionsForUsers();
+    await mongoose.connect(process.env.MONGO_URI_DEV);
+    await seedPlans();
+    await seedCollectionsForUsers();
     console.log("Base de datos on");
   } catch (error) {
     console.log("error", error);
--- v1/routes/libraryItem.routes.js
+++ v1/routes/libraryItem.routes.js
@@ -5,8 +5,8 @@ import {
   getLibraryItemByIdController,
   getLibraryItemsByCollectionController,
   getLibraryItemsByUserController,
-  updateLibraryItemProgresoController,
   updateLibraryItemEstadoController,
+  updateLibraryItemProgresoController,
 } from "../controllers/libraryItem.controller.js";
 import { validate } from "../middlewares/validate.js";
 import { libraryItemSchema } from "../validators/libraryItem.validator.js";
@@ -24,7 +24,7 @@ router.delete("/:itemId", deleteLibraryItemController);
 router.get("/:itemId", getLibraryItemByIdController);
 
 // Endpoint para actualizar el progreso de un libraryItem específico
-
+router.patch("/:itemId/progreso", updateLibraryItemProgresoController);
 // Endpoint para actualizar el estado de un libraryItem específico
 router.patch("/:itemId/estado", updateLibraryItemEstadoController);
 export default router;
