00fbf1dc45e32456f05553cda0954ab25c9427c9
MacDuki <pcmyact@hotmail.com>
2025-10-09 18:11:31 -0300

    ..

--- v1/middlewares/validate.js
+++ v1/middlewares/validate.js
@@ -2,22 +2,41 @@ export function validate(schemas) {
   return async (req, res, next) => {
     try {
       if (schemas.body) {
-        req.body = await schemas.body.validateAsync(req.body, {
+        const value = await schemas.body.validateAsync(req.body, {
           abortEarly: false,
           stripUnknown: true,
         });
+        if (req.body && typeof req.body === "object") {
+          for (const k of Object.keys(req.body)) delete req.body[k];
+          Object.assign(req.body, value);
+        } else {
+          req.body = value;
+        }
       }
       if (schemas.params) {
-        req.params = await schemas.params.validateAsync(req.params, {
+        const value = await schemas.params.validateAsync(req.params, {
           abortEarly: false,
           stripUnknown: true,
         });
+        if (req.params && typeof req.params === "object") {
+          for (const k of Object.keys(req.params)) delete req.params[k];
+          Object.assign(req.params, value);
+        } else {
+          req.params = value;
+        }
       }
       if (schemas.query) {
-        req.query = await schemas.query.validateAsync(req.query, {
+        const value = await schemas.query.validateAsync(req.query, {
           abortEarly: false,
           stripUnknown: true,
         });
+
+        if (req.query && typeof req.query === "object") {
+          for (const k of Object.keys(req.query)) delete req.query[k];
+          Object.assign(req.query, value);
+        } else {
+          req.validated = Object.assign({}, req.validated, { query: value });
+        }
       }
       return next();
     } catch (err) {
